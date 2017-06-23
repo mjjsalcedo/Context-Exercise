ceeb3057f188cc2f7c16142e35cad427739c487b
Mariel Salcedo <Mariel Salcedo>
2017-06-22 14:47:04 -1000

    #1 done

--- context.js
+++ context.js
@@ -17,10 +17,10 @@ var wall_e = new Robot("Wall-E");
 
 
 // #1
-console.log(  );
+console.log( add(0,1) );
 
-// #2  
-console.log(  );
+// #2
+console.log( Robot.add.call(voltron) );
 
 // #3
 console.log(  );
